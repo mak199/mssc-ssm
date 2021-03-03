f35557d705cfbac9b9cf4ce8110dbe60f0d3287a
mak199 <mak.khan199@googlemail.com>
2021-03-03 13:01:13 +0100

    progressing state from new to pre-auth in db

---
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.msscssm.services;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.statemachine.StateMachine;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PaymentServiceImplTest {
+
+    @Autowired
+    PaymentService paymentService;
+    Payment payment;
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @BeforeEach
+    void setUp() {
+        payment = Payment.builder().amount(new BigDecimal(12.00)).build();
+    }
+
+    @Transactional
+    @Test
+    void preAuth() {
+        Payment savedPayment = paymentService.newPayment(payment);
+        System.out.println("Should be NEW");
+        System.out.println(savedPayment.getState());
+        StateMachine<PaymentState, PaymentEvent> sm = paymentService.preAuth(savedPayment.getId());
+        Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
+        System.out.println("Should be PRE_AUTH");
+        System.out.println(sm.getState().getId());
+        System.out.println(preAuthedPayment);
+    }
+}
